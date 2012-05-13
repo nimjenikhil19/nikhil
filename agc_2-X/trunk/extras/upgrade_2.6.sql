97f140065f753a87c0e3041367bc8b5f5ce167a4
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2012-05-13 05:01:03 +0000

    Added options to allow for an agent to place manual dial calls out as if they were 	in-group inbound calls. Also allows for no-dial calls that are not actually placed out the telco, but are logged as calls. For more information see the "In-Group Manual Dial" options in Campaign Detail

--- agc_2-X/trunk/extras/upgrade_2.6.sql
+++ agc_2-X/trunk/extras/upgrade_2.6.sql
@@ -1,10 +1,9 @@
 UPDATE system_settings SET db_schema_version='1318',version='2.6b0.5',db_schema_update_date=NOW() where db_schema_version < 1318;
 
-
-
-
+ALTER TABLE vicidial_phone_codes MODIFY geographic_description VARCHAR(100);
 
 ALTER TABLE vicidial_campaigns ADD in_group_dial ENUM('DISABLED','MANUAL_DIAL','NO_DIAL','BOTH') default 'DISABLED';
-ALTER TABLE vicidial_campaigns ADD in_group_dial_select ENUM('AGENT_SELECTED','CAMPAIGN_SELECTED','ALL_USER_GROUP') default 'AGENT_SELECTED';
+ALTER TABLE vicidial_campaigns ADD in_group_dial_select ENUM('AGENT_SELECTED','CAMPAIGN_SELECTED','ALL_USER_GROUP') default 'CAMPAIGN_SELECTED';
 
 UPDATE system_settings SET db_schema_version='1319',db_schema_update_date=NOW() where db_schema_version < 1319;
+
