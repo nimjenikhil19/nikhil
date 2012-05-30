a1a58ab81a293c2d611bbe9db140a821a8c1a9d4
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2012-05-30 14:21:16 +0000

    Added Safe Harbor Audio Field to campaign options which allows for a lead field to be used as an override safe harbor audio on a per-lead basis

--- extras/MySQL_AST_CREATE_tables.sql
+++ extras/MySQL_AST_CREATE_tables.sql
@@ -851,7 +851,8 @@ callback_list_calltime ENUM('ENABLED','DISABLED') default 'DISABLED',
 user_group VARCHAR(20) default '---ALL---',
 hopper_vlc_dup_check ENUM('Y','N') default 'N',
 in_group_dial ENUM('DISABLED','MANUAL_DIAL','NO_DIAL','BOTH') default 'DISABLED',
-in_group_dial_select ENUM('AGENT_SELECTED','CAMPAIGN_SELECTED','ALL_USER_GROUP') default 'CAMPAIGN_SELECTED'
+in_group_dial_select ENUM('AGENT_SELECTED','CAMPAIGN_SELECTED','ALL_USER_GROUP') default 'CAMPAIGN_SELECTED',
+safe_harbor_audio_field VARCHAR(30) default 'DISABLED'
 );
 
 CREATE TABLE vicidial_lists (
@@ -2812,7 +2813,7 @@ CREATE TABLE vicidial_log_noanswer_archive LIKE vicidial_log_noanswer;
 CREATE TABLE vicidial_did_agent_log_archive LIKE vicidial_did_agent_log; 
 CREATE UNIQUE INDEX vdala on vicidial_did_agent_log_archive (uniqueid,call_date,did_route);
 
-UPDATE system_settings SET db_schema_version='1320',db_schema_update_date=NOW();
+UPDATE system_settings SET db_schema_version='1321',db_schema_update_date=NOW();
 
 GRANT RELOAD ON *.* TO cron@'%';
 GRANT RELOAD ON *.* TO cron@localhost;
