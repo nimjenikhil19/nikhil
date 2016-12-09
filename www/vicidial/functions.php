b9cb21f320841d467bd7892221d8085613d0c5cd
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2016-12-09 12:07:30 +0000

    fix for recent report

--- www/vicidial/functions.php
+++ www/vicidial/functions.php
@@ -1334,4 +1334,23 @@ function hex2rgb($hex) {
 	return $rgb; // returns an array with the rgb values
 }
 
+function createDateRangeArray($strDateFrom,$strDateTo)
+	{
+	$aryRange=array();
+
+	$iDateFrom=mktime(1,0,0,substr($strDateFrom,5,2),     substr($strDateFrom,8,2),substr($strDateFrom,0,4));
+	$iDateTo=mktime(1,0,0,substr($strDateTo,5,2),     substr($strDateTo,8,2),substr($strDateTo,0,4));
+
+	if ($iDateTo>=$iDateFrom)
+	{
+		array_push($aryRange,date('Y-m-d',$iDateFrom)); // first entry
+		while ($iDateFrom<$iDateTo)
+		{
+			$iDateFrom+=86400; // add 24 hours
+			array_push($aryRange,date('Y-m-d',$iDateFrom));
+		}
+	}
+	return $aryRange;
+	}
+
 ?>
