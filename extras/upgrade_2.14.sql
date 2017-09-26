91d9fcd2c1384b25eb1bc066852cef0e2fe06710
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2017-09-26 22:01:32 +0000

    Added in_filter_override option and cache searching option to DNCcom inbound filter script

--- extras/upgrade_2.14.sql
+++ extras/upgrade_2.14.sql
@@ -389,3 +389,7 @@ index(scrub_date)
 ) ENGINE=MyISAM;
 
 UPDATE system_settings SET db_schema_version='1517',db_schema_update_date=NOW() where db_schema_version < 1517;
+
+ALTER TABLE vicidial_dnccom_scrub_log ADD flag_projdnc ENUM('','0','1') default '' AFTER flag_dnc;
+
+UPDATE system_settings SET db_schema_version='1518',db_schema_update_date=NOW() where db_schema_version < 1518;
