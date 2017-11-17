e675fb90914e19c8c50f77da3d7321517a297df2
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2017-11-17 06:52:37 +0000

    Added the ability to include duplicate custom text fields in a custom fields  form in the agent screen.

--- agc_2-X/trunk/extras/upgrade_2.14.sql
+++ agc_2-X/trunk/extras/upgrade_2.14.sql
@@ -429,3 +429,7 @@ ALTER TABLE vicidial_callbacks_archive ADD email_result ENUM('SENT','FAILED','NO
 INSERT INTO vicidial_settings_containers(container_id,container_notes,container_type,user_group,container_entry) VALUES ('AGENT_CALLBACK_EMAIL ','Scheduled callback email alert settings','OTHER','---ALL---','; sending email address\r\nemail_from => vicidial@local.server\r\n\r\n; subject of the email\r\nemail_subject => Scheduled callback alert for --A--agent_name--B--\r\n\r\nemail_body_begin => \r\nThis is a reminder that you have a scheduled callback right now for the following lead:\r\n\r\nName: --A--first_name--B-- --A--last_name--B--\r\nPhone: --A--phone_number--B--\r\nAlt. phone: --A--alt_phone--B--\r\nEmail: --A--email--B--\r\nCB Comments: --A--callback_comments--B--\r\nLead Comments: --A--comments--B--\r\n\r\nPlease don\'t respond to this, fool.\r\n\r\nemail_body_end');
 
 UPDATE system_settings SET db_schema_version='1523',db_schema_update_date=NOW() where db_schema_version < 1523;
+
+ALTER TABLE vicidial_lists_fields ADD field_duplicate ENUM('Y','N') default 'N';
+
+UPDATE system_settings SET db_schema_version='1524',db_schema_update_date=NOW() where db_schema_version < 1524;
