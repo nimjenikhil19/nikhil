522fd026691c5aa464b55dd01831df4051a2b794
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2018-03-30 19:39:19 +0000

    small fix for list_download.php

--- agc_2-X/trunk/www/vicidial/list_download.php
+++ agc_2-X/trunk/www/vicidial/list_download.php
@@ -514,6 +514,7 @@ while ($i < $leads_to_print)
 		}
 	elseif ($download_type == 'cidgroup')
 		{
+		if ($row[3]=='') {$row[3]='N';}
 		$row_data[$i] .= "$row[0],$row[1],$row[2],$row[3]";
 		}
 	else
