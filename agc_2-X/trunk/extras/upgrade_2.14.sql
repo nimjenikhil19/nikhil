1b8ffe05678c3595b89f30ebcdc80e631f30f42f
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2019-02-23 15:36:11 +0000

    Added options to define the static prompts to use for the Inbound Group "Play Place in Line" feature

--- agc_2-X/trunk/extras/upgrade_2.14.sql
+++ agc_2-X/trunk/extras/upgrade_2.14.sql
@@ -799,3 +799,8 @@ index(call_date)
 CREATE TABLE vicidial_sessions_recent_archive LIKE vicidial_sessions_recent;
 
 UPDATE system_settings SET db_schema_version='1563',db_schema_update_date=NOW() where db_schema_version < 1563;
+
+ALTER TABLE vicidial_inbound_groups ADD place_in_line_caller_number_filename TEXT;
+ALTER TABLE vicidial_inbound_groups ADD place_in_line_you_next_filename TEXT;
+
+UPDATE system_settings SET db_schema_version='1564',db_schema_update_date=NOW() where db_schema_version < 1564;
