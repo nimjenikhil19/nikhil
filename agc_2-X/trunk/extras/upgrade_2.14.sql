5b83d4f973da76466db5c67054e4270e34168e4a
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2019-05-30 14:16:35 +0000

    Added list_status_modification_confirmation feature

--- agc_2-X/trunk/extras/upgrade_2.14.sql
+++ agc_2-X/trunk/extras/upgrade_2.14.sql
@@ -849,3 +849,7 @@ ALTER TABLE system_settings ADD user_admin_redirect ENUM('1','0') default '0';
 ALTER TABLE vicidial_users ADD user_admin_redirect_url TEXT;
 
 UPDATE system_settings SET db_schema_version='1569',db_schema_update_date=NOW() where db_schema_version < 1569;
+
+ALTER TABLE system_settings ADD list_status_modification_confirmation ENUM('1','0') default '0';
+
+UPDATE system_settings SET db_schema_version='1570',db_schema_update_date=NOW() where db_schema_version < 1570;
