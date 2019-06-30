0883a449a0b12633f976ce614455303026758ad7
mattf <mattf@3d104415-ff17-0410-8863-d5cf3c621b8a>
2019-06-30 19:47:25 +0000

    small fix for agent time report on agent screen

--- www/agc/vdc_db_query.php
+++ www/agc/vdc_db_query.php
@@ -17803,7 +17803,7 @@ if ($ACTION == 'AGENTtimeREPORT')
 				}
 
 			$days=-1;
-			if ($start_date == "$end_date")
+			if ($start_date == "$NOW_DATE")
 				{
 				$days=0;
 				$temp_date_epoch = $secX;
